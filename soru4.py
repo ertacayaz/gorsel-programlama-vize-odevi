61d53915462a8a24576234357679ee94a4b61154
Ertaç AYAZ <74978897+ertacayaz@users.noreply.github.com>
2020-12-13 16:31:11 +0300

    Update soru4.py

--- soru4.py
+++ soru4.py
@@ -1,3 +1,5 @@
+a = 0
+
 for i in range(100,999):
     x = str(i)[0]
     y = str(i)[1]
@@ -5,3 +7,6 @@ for i in range(100,999):
 
     if x !=y and x!= z and y != z:
         print(i)
+        a += 1
+
+print("Toplam: {}".format(str(a)))
